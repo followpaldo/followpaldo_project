f7c59344e7e707dc62c552217bd5199cd7d02249
kimsolji <ggg454538@gmail.com>
2024-04-08 17:14:06 +0900

    camplist만 완벽

--- src/main/java/jjon/gangsan/FollowgangsanApplication.java
+++ src/main/java/jjon/gangsan/FollowgangsanApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 @SpringBootApplication
-public class PollowGangsanApplication {
+public class FollowgangsanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PollowGangsanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FollowgangsanApplication.class, args);
+    }
 
 }
+
+
