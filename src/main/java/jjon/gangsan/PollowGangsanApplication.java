88d7c7efd886e3c8cfab1f1c632612f910002f62
kimsolji <ggg454538@gmail.com>
2024-04-08 19:13:35 +0900

    camplist240408마지막

--- src/main/java/jjon/gangsan/PollowGangsanApplication.java
+++ src/main/java/jjon/gangsan/PollowGangsanApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication
-public class FollowgangsanApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FollowgangsanApplication.class, args);
-    }
+@SpringBootApplication
+public class PollowGangsanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollowGangsanApplication.class, args);
+	}
 
 }
-
-
