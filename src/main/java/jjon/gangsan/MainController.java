f9333abcce494a3e79308a8574bb90dea542cb1a
owencity <151708233+owencity@users.noreply.github.com>
2024-05-03 17:35:10 +0900

    Update MainController.java

--- src/main/java/jjon/gangsan/MainController.java
+++ src/main/java/jjon/gangsan/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	
 	@GetMapping("/views")
-	public String test() {
+	public String mainPage() {
 		return "MainPage";
 	}
 }
